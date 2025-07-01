4ea52a62db3962169c0f17aa79573d93b38784c1
zhang he <he.zhang@neuetech.cn>
2025-07-01 18:17:42 +0800

    no message

---
+++ src/pages/index/recruitment/recruitment.vue
@@ -0,0 +1,17 @@
+<route lang="json5" type="page">
+{
+  style: {
+    navigationBarTitleText: '招聘',
+    navigationStyle: 'custom',
+  },
+}
+</route>
+<template>
+  <view class="recruitment-container"></view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
